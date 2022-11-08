1e65fa4968c2632f91bf7a65ac0a9a3f9932216c
JordanWitt <wittjordan94@gmail.com>
2022-11-08 10:16:05 -0600

    Java syntax, types, and variable

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,47 @@ public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.print("Hello, World!");
         System.out.print(" Hello");
+        System.out.println(" Code inside of curly braces should be indented");
+        System.out.println("with four spaces");
+
+         int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+          int n = 5;
+          System.out.println(++n);
+          System.out.println(n);
+
+          int myFavoriteNumber = 8;
+          System.out.println(myFavoriteNumber);
+
+          String myString = "Jordan Witt";
+          System.out.println(myString);
+
+          long myNumber = 123;
+          System.out.println(myNumber);
+
+          double myNumber2 = 3.14;
+          System.out.println(myNumber2);
+
+          int four = 4;
+          four += 5;
+          System.out.println(four);
+
+          four -= 20;
+          System.out.println(four);
+
+          int three = 3;
+          int five = 5;
+          five = three * five;
+          System.out.println(five);
+
+          int ten = 10;
+          int two = 2;
+          ten = ten / two;
+          two = two - ten;
+          System.out.println(ten);
+          System.out.println(two);
     }
 }
+
