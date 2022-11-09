1d37af4ed36a6b109619873f65848cd31ae64184
JordanWitt <wittjordan94@gmail.com>
2022-11-09 09:18:50 -0600

    console IO exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.nextLine();
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+
+        double pi = 3.14159;
+        System.out.println("Pi equals: --> \"" + pi + "\" <--");
+
+//        String name = "codeup";
+//        System.out.printf("Hello there, %s. Nice to see you.%n", name);
+
+        String greeting = "Salutations";
+        String name = "codeup";
+        System.out.printf("%s, %s!", greeting, name);
+        scanner.nextLine();
+
+        System.out.println("Enter a number");
+        Integer userInput2 = scanner.nextInt();
+        System.out.println("You entered: --> \"" + userInput2 + "\" <--");
+
+        scanner.nextLine();
+
+        String greeting2 = "How are you feeling today?";
+        String response1 = "Yikes, could be better...what do you want?";
+        String out1 = "I don't know how to do that for you...well I'm done here.";
+        System.out.printf("%s", greeting2);
+        scanner.nextLine();
+        System.out.printf("%s", response1);
+        scanner.nextLine();
+        System.out.printf("%s.", out1);
+
+        scanner.nextLine();
+
+        System.out.print("Enter the width and length of you room, STAT");
+        float nextString =scanner.nextFloat();
+        float nextString2 = scanner.nextFloat();
+        float total = (nextString * nextString2);
+        System.out.println("The area of your room is " + total);
+
+    }
+}
+
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,44 +6,44 @@ public static void main(String[] args) {
         System.out.println(" Code inside of curly braces should be indented");
         System.out.println("with four spaces");
 
-         int x = 5;
-        System.out.println(x++);
-        System.out.println(x);
+          int x = 5;
+            System.out.println(x++);
+            System.out.println(x);
 
           int n = 5;
-          System.out.println(++n);
-          System.out.println(n);
+            System.out.println(++n);
+            System.out.println(n);
 
           int myFavoriteNumber = 8;
-          System.out.println(myFavoriteNumber);
+             System.out.println(myFavoriteNumber);
 
           String myString = "Jordan Witt";
-          System.out.println(myString);
+             System.out.println(myString);
 
           long myNumber = 123;
-          System.out.println(myNumber);
+             System.out.println(myNumber);
 
           double myNumber2 = 3.14;
-          System.out.println(myNumber2);
+             System.out.println(myNumber2);
 
           int four = 4;
           four += 5;
-          System.out.println(four);
+             System.out.println(four);
 
           four -= 20;
-          System.out.println(four);
+             System.out.println(four);
 
           int three = 3;
           int five = 5;
           five = three * five;
-          System.out.println(five);
+              System.out.println(five);
 
           int ten = 10;
           int two = 2;
           ten = ten / two;
           two = two - ten;
-          System.out.println(ten);
-          System.out.println(two);
+              System.out.println(ten);
+              System.out.println(two);
     }
 }
 
