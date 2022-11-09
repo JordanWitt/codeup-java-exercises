7acb6c3752492b02ff986e8349688b26a3ec5bd8
JordanWitt <wittjordan94@gmail.com>
2022-11-09 12:29:42 -0600

    control flow ex 1

---
+++ src/controlFlowExercises.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class controlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 10) {
+            System.out.println(i++);
+        }
+
+
+
+    }
+}
