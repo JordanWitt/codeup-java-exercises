00ead537ff6d97aa5c80944360eaf895f0b8c76f
JordanWitt <wittjordan94@gmail.com>
2022-11-10 14:53:29 -0600

    starting the text adventure game

--- src/Bob.java
+++ src/Bob.java
@@ -1,27 +1,41 @@
 import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
+//
+//        String cont;
+//        do { System.out.print("Hello, I'm AI Bob: ");
+//            Scanner scanner = new Scanner(System.in);
+//
+//            String userInput = scanner.nextLine();
+//            int strLength = userInput.length();
+//
+//
+//            if (userInput.endsWith("?")) {
+//                System.out.println("Sure");
+//            } else if (userInput.endsWith("!")) {
+//                System.out.println("Whoa, chill out!");
+//            } else if (strLength == 0) {
+//                System.out.println("Fine. Be that way!");
+//            } else {System.out.println("Whatever");}
+//
+//            System.out.print("Want to continue (Y/N): ");
+//
+//            cont = scanner.next();
+//        } while (cont.equalsIgnoreCase("y"));
 
-        String cont;
-        do { System.out.print("Hello, I'm AI Bob: ");
-            Scanner scanner = new Scanner(System.in);
-
-            String userInput = scanner.nextLine();
-            int strLength = userInput.length();
-
-
-            if (userInput.endsWith("?")) {
-                System.out.println("Sure");
-            } else if (userInput.endsWith("!")) {
-                System.out.println("Whoa, chill out!");
-            } else if (strLength == 0) {
-                System.out.println("Fine. Be that way!");
-            } else {System.out.println("Whatever");}
-
-            System.out.print("Want to continue (Y/N): ");
-
-            cont = scanner.next();
-        } while (cont.equalsIgnoreCase("y"));
-
+//        String count;
+//        String userInput = scanner.nextLine();
+//        int stringLength = userInput.length();
+//        do {
+//            System.out.println("You wake up in a in a field and see a vehicle in the distance: press s to move forward");
+//            Scanner scanner = new Scanner(System.in);
+//
+//            if(userInput == ("s")) {
+//                System.out.println("The truck appears to be abandoned, but there are no keys in sight, would you like to attempt to wire start? (Y/N)");
+//            }
+//            count = scanner.next();
+//        }while (count.equalsIgnoreCase("y, n"));
+//    }
+//}
     }
 }
---
+++ src/textAdventure.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+public class textAdventure {
+    public static void main(String[] args) {
+    // System objects
+    Scanner in = new Scanner(System.in);
+    Random rand = new Random();
+
+    //Game variables
+     String() enemies = { "Skeleton", "Zombie", "Warrior", "Assassin" }; // type of enemies
+     int maxEnemyHealth = 75; //enemy starting health
+     int enemyAttackDamage = 25; // enemy attack damage
+
+     //Player variables
+
+      int health = 100; //player starting health
+      int attackDamage = 50; //player attack damage
+      int numHealthPotions = 3; //amount of health potions
+      int healthPotionHealAmoutn = 30; //heal amount for potions
+      int healthPotionDropChance = 50; // 50 percent chance enemy drops a health potion
+
+    }
+}
