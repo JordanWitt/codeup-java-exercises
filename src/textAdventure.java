c64aba6369cfd813123e86ea3b445f0ac7431ecc
JordanWitt <wittjordan94@gmail.com>
2022-11-14 09:50:20 -0600

    added more System out.ptrinln

--- src/textAdventure.java
+++ src/textAdventure.java
@@ -39,13 +39,64 @@ public class textAdventure {
               System.out.println("\t1 Attack");
               System.out.println("\t2 Drink health potion");
               System.out.println("\t3 Run!"); //player options
-              System.out.println("\tHint: Your starting HP is full");
 
+              //if user input = 1
               String input = in.nextLine(); //grabs next line from console/ user input
+              if(input.equals("1")) {
+                int damageDealt = rand.nextInt(attackDamage);
+                int damageTaken = rand.nextInt(enemyAttackDamage);
+
+                enemyHealth -= damageDealt;
+                health -= damageTaken; //health goes down
+
+                  System.out.println("\t You strike the " + enemy + " for " + damageDealt + " damage");
+                  System.out.println("\t You recieve " + damageTaken + " in retaliation!");
+
+                  if(health <= 1) {
+                      System.out.println("\t You have taken too much damage, you are too weak to continue on!");
+                      break;
+                  }
+              }
+              // if user input = 2
+              else if(input.equals("2")) {
+                  if(numHealthPotions > 0) {
+                      health += healthPotionHealAmoutn;
+                      numHealthPotions--;
+                      System.out.println("\t > You drink a health potion, healing yourself for " +
+                              healthPotionHealAmoutn + ". " + "\n\t> You now have" + health + "HP"
+                              + "\n\t> You have" + numHealthPotions + " health potions left.\n");// health potion output
+                  }
+                  // system out print when the user runs out of potions
+                  else {
+                      System.out.println("\t> You have no health potions left! Defeat enemies for a change to get one");
+                  }
+
+              }
+              // if user input = 3
+              else if(input.equals("3")) {
+                  System.out.println("\t You run away from the " + enemy + "!");
+                  continue GAME; //brings it back to the START of loop vs the attack iteration
+              }
+              // if the user input is not one of the options listed
+              else {
+                  System.out.println("\tInvalid command");
+
+              }
           }
+          // break out of second loop if user can't continue
+          if(health < 1) {
+              System.out.println("You limp out of the dungeon, weak from battle.");
+              break;
+          }
+          System.out.println("------------------------------------------------------");
+          System.out.println(" # " + enemy + " was defeated! # ");
+          System.out.println(" # You have " +health + " HP left. # " );
+          if(rand.nextInt(100) < healthPotionDropChance) { // generating random # between 0/100 50% chance
 
+              numHealthPotions++;
+              System.out.println(" # The " + enemy + " dropped a health potion! #");
 
+          }
       }
-
     }
 }
