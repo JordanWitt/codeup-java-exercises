56cc8f35fee078af70acca17d342f56dc1df775f
JordanWitt <wittjordan94@gmail.com>
2022-11-14 17:51:10 -0600

    game 2

---
+++ src/textAdventure2.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+public class textAdventure2 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+
+        String evilHomework[] = {"Html", "CSS" , "JavaScript", "Java", "Spring" , "Git", "Something difficult"};
+        int maxEvilHomeworkHealth = 90;
+        int whatIDontKnow = 60;
+
+        // user
+        int attempts = 1;
+        int knowledge = 70;
+        int attackDamage = 50;
+        int homeworkRedDo = 1;
+        int homeworkSuccess = 30;
+        int homeworkReDoChance = 50;
+
+        boolean struggling = true;
+
+        System.out.println("^(#｀∀ ´)_Ψ-----------Welcome to Codeup-----------∋━━o(｀∀ ´oメ）～→");
+
+    }
+}
