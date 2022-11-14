93eb3e4cef50fbdbc797b848d1ceb3f3ec2966fb
JordanWitt <wittjordan94@gmail.com>
2022-11-14 16:33:11 -0600

    question 5

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -107,8 +107,43 @@ public class MethodsExercises {
             attempt++;
         }
         System.out.println("Thanks for playing dice game, you rolled the dice " + attempt + " times!");
+
+        // question 5
+        int answer, guess;
+        // maximum value is 100
+        final int MAX = 100;
+        // takes input using scanner
+        Scanner in2 = new Scanner(System.in);
+        // Random instance
+        Random rand = new Random();
+        boolean correct = false;
+        // correct answer
+        answer = rand.nextInt(MAX) + 1; // generating random number
+        // loop until the guess is correct
+        while (!correct) {
+            System.out.println(
+                    "Guess a number between 1 and 100: ");
+            // guess value
+            guess = in2.nextInt();
+            // if guess is greater than actual
+            if (guess > answer) {
+                System.out.println("Too high, try again");
+            }
+            // if guess is less than actual
+            else if (guess < answer) {
+                System.out.println("Too low, try again");
+            }
+            // guess is equal to actual value
+            else {
+                System.out.println(
+                        "Yes, you guessed the number.");
+                correct = true;
+            }
+        }
+        System.exit(0);
+        }
     }
-}
+
 
 
 
