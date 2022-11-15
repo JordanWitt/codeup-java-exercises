5a024c3b6d3f294c16d728e36888acfa8f46c012
JordanWitt <wittjordan94@gmail.com>
2022-11-14 18:21:24 -0600

    added more

--- src/textAdventure2.java
+++ src/textAdventure2.java
@@ -5,7 +5,7 @@ public class textAdventure2 {
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
 
-        String evilHomework[] = {"Html", "CSS" , "JavaScript", "Java", "Spring" , "Git", "Something difficult"};
+        String[] evilHomework = {"Html", "CSS" , "JavaScript", "Java", "Spring" , "Git", "Something difficult"};
         int maxEvilHomeworkHealth = 90;
         int whatIDontKnow = 60;
 
@@ -20,6 +20,35 @@ public class textAdventure2 {
         boolean struggling = true;
 
         System.out.println("^(#｀∀ ´)_Ψ-----------Welcome to Codeup-----------∋━━o(｀∀ ´oメ）～→");
+
+        GAME:
+        while(struggling){
+            int homeworkHealth = rand.nextInt(maxEvilHomeworkHealth);
+            String homework = evilHomework[rand.nextInt(evilHomework.length)];
+
+            System.out.println("Oh no! " + homework + " is here!");
+            while (homeworkHealth > 0){
+                System.out.println("Your current knowledge " + knowledge + "!");
+                System.out.println(homework + " homework difficulty " +homeworkHealth);
+                System.out.println("What would you like to do?");
+                System.out.println("1. ATTACK with more studying");
+                System.out.println("2. Practice for better results");
+                System.out.println("3. Leave it all behind, this is not my battle");
+
+                String input = in.nextLine();
+                if(input.equals("1")){
+                    int knowledgeLearned = rand.nextInt(attackDamage);
+                    int knowledgeMisunderstood = rand.nextInt(whatIDontKnow);
+
+                    System.out.println("You have gained " + knowledgeLearned + "!");
+                    System.out.println("Brain is becoming cloudy with " + knowledgeMisunderstood + " misunderstandings");
+
+                    knowledge -= knowledgeMisunderstood;
+                    homeworkHealth -= knowledgeLearned;
+
+                }
+            }
+        }
 
     }
 }
