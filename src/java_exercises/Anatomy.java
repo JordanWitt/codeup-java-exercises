e2de970f7026a89056b5ea51d463a6a4e46d7831
JordanWitt <wittjordan94@gmail.com>
2023-03-03 14:42:20 -0600

    notes

---
+++ src/java_exercises/Anatomy.java
@@ -0,0 +1,10 @@
+package java_exercises;
+
+public class Anatomy {
+    //class ^ contains one or more functions that are related
+    public static void main(String[] args) {
+// where we write method in a class
+    }
+    //classes should all have PascalNamingConvention
+    //methods should all have camelNamingConvention
+}
--- src/java_exercises/Anatonmy.java
+++
@@ -1,7 +0,0 @@
-package java_exercises;
-
-public class Anatonmy {
-    public static void main(String[] args) {
-
-    }
-}
